6a133005f824f44d9075713dd573cfe2c24d93c5
RafoDev <rafael.david@ucsp.edu.pe>
2023-04-10 00:03:37 -0500

    feat (scanner) : getToken function works for keywords and IDs. It still needs to work for literals and operators/delimiters.

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <regex>
 #include <fstream>
 #include <stack>
@@ -12,150 +13,160 @@ using namespace std;
 
 char currChar = ' ';
 
-struct Tokens
-{
-    string identifiersRegExp;
-    vector<string> operators;
-    map<string, string> delimiters;
-    Tokens()
-    {
-        operators = {
-            "+", "-", "*", "%",
-            "<", ">", "<=", ">=", "==", "=", ":"};
-        // delimiters["("] = "OPEN_PAR"
-    }
+vector<string> keywords = {
+		"False", "None", "True", "and", "as", "assert", "async", "await", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try", "while", "with", "yield"};
+vector<string> operators = {
+		"+", "-", "*", "%", "/", "<",
+		">", "<=", ">=", "=="};
+vector<string> delimiters = {
+		")",
+		"(",
+		"[",
+		"]",
+		",",
+		".",
+		";",
+		"=",
+		":",
+		"->",
+		"=>",
 };
-// regex identifiersReg("[a-z|A-Z][a-z|A-Z|0-9|_]*");
-// vector<string> operators = ;
-// map<string, string> delimiters;
-// delimiters["("] = "OPEN_PAR";
-// delimiters[")"] = "CLOSE_PAR";
+vector<string> comments = {"//", "#"};
 
-/*
-Token categories
-    Line structure
-    Identifiers
-    Keywords
-    Literals
-    Operators
-    Delimiters
-*/
+regex idRegexp("[a-z|A-Z][a-z|A-Z|0-9|_]*");
 
-// struct tokens{
-
-// };
+struct MToken
+{
+	string type;
+	string value;
+};
 
 void showMessage(string type, string tokenType, string token, int lineNumber, int pointer)
 {
-    string message = type + " SCAN - ";
-    message += tokenType;
-    message += " [";
-    message += token;
-    message += "] found at (";
-    message += to_string(lineNumber);
-    message += ":";
-    message += to_string(pointer);
-    message += ")\n";
-    cout << message;
+	string message = type + " SCAN - ";
+	message += tokenType;
+	message += " [";
+	message += token;
+	message += "] found at (";
+	message += to_string(lineNumber);
+	message += ":";
+	message += to_string(pointer);
+	message += ")\n";
+	cout << message;
 }
 
-bool RegexMatcher(string str, regex reg)
-{
-    return regex_match(str, reg);
-}
-
-// info: get the char and move the pointer
 char getChar(string &line, int &pointer)
 {
-    return line[++pointer];
+	return line[++pointer];
 }
-// info: get the char without moving the pointer
+
 char peekChar(string &line, int pointer)
 {
-    return line[pointer + 1];
+	return line[pointer + 1];
 }
 
 int computeIndent(string &line, int &pointer, int endLine)
 {
-    int indent = 0;
-    currChar = getChar(line, pointer);
+	int indent = 0;
+	currChar = getChar(line, pointer);
 
-    while ((pointer < endLine) && (int(currChar) == 32 || int(currChar) == 9))
-    {
-        currChar = getChar(line, pointer);
-        indent++;
-    }
+	while ((pointer < endLine) && (int(currChar) == 32 || int(currChar) == 9))
+	{
+		currChar = getChar(line, pointer);
+		indent++;
+	}
 
-    return indent;
+	return indent;
 }
 
-// string parseIdentifier(string &line, int &pointer, int endLine)
-// {
-//     string identifier = "";
-//     char currChar = peekChar(line, pointer);
-//     string currString(1, currChar);
-//     while ((pointer < endLine) && !(int(currChar) == 32 || int(currChar) == 9) && (regex_match(currString, identifiersReg) || currChar == '_'))
-//     {
-//         // cout << regex_match(currString, identifiersReg) << " " << currString;
-//         identifier += currChar;
-//         // cout << currChar << ' ' << int(currChar) << "\n";
-//         pointer++;
-//         currChar = peekChar(line, pointer);
-//         currString.assign(currChar, currChar + 1);
-//     }
-//     return identifier;
-// }
-
-void parse(string filename)
+MToken getToken(string &line, int &pointer, int endLine)
 {
-    fstream file;
-    file.open(filename, ios::in);
-    int lineNumber = 1;
-    cout << "INFO SCAN - Start scanning...\n";
+	MToken token;
+	string tokenValue = "";
+	string charStr(1, currChar);
 
-    if (file.is_open())
-    {
-        string line;
-        priority_queue<int> indentStack;
-        indentStack.push(0);
+	while (
+			(pointer < endLine) &&
+			!(int(currChar) == 32 || int(currChar) == 9) &&
+			(find(operators.begin(), operators.end(), charStr) == operators.end()) &&
+			(find(delimiters.begin(), delimiters.end(), charStr) == delimiters.end()))
+	{
 
-        while (getline(file, line))
-        {
-            int pointer = -1;
-            int endLine = line.length();
-            int currInden = computeIndent(line, pointer, endLine);
+		tokenValue += currChar;
+		currChar = getChar(line, pointer);
+		charStr.assign(1, currChar);
+	}
 
-            if (currInden > indentStack.top())
-            {
-                indentStack.push(currInden);
-                showMessage("DEBUG", "INDENT", "", lineNumber, pointer / 4);
-            }
-            while (currInden < indentStack.top())
-            {
-                indentStack.pop();
-                showMessage("DEBUG", "DEDENT", "", lineNumber, pointer / 4);
-            }
+	token.value = tokenValue;
+	token.type = "ANY";
+	if (regex_match(tokenValue, idRegexp))
+		token.type = "ID";
+	else
+		token.type = "KEY";
 
-            // while (pointer < endLine)
-            // {
-            //     string identifier = parseIdentifier(line, pointer, endLine);
-            //     // if (identifier.length())
-            //     showMessage("DEBUG", "ID", identifier, lineNumber, pointer);
+	// while ((pointer < endLine) && !(int(currChar) == 32 || int(currChar) == 9) && (regex_match(currString, identifiersReg) || currChar == '_'))
+	// {
+	// 	// cout << regex_match(currString, identifiersReg) << " " << currString;
+	// 	identifier += currChar;
+	// 	// cout << currChar << ' ' << int(currChar) << "\n";
+	// 	pointer++;
+	// 	currChar = peekChar(line, pointer);
+	// 	currString.assign(currChar, currChar + 1);
+	// }
+	return token;
+}
 
-            //     char currChar = peekChar(line, pointer);
-            //     // cout << currChar << ' ' << int(currChar) << "\n";
-            //     pointer++;
-            // }
-            cout << "------------NEWLINE----------------\n";
-            // showMessage("DEBUG", "NEWLINE", "", lineNumber, pointer);
-            lineNumber++;
-        }
-    }
+void scan(string filename)
+{
+	fstream file;
+	file.open(filename, ios::in);
+	int lineNumber = 1;
+	cout << "INFO SCAN - Start scanning...\n";
 
-    file.close();
+	if (file.is_open())
+	{
+		string line;
+		priority_queue<int> indentStack;
+		indentStack.push(0);
+
+		while (getline(file, line))
+		{
+			int pointer = -1;
+			int endLine = line.length();
+			int currInden = computeIndent(line, pointer, endLine);
+
+			if (currInden > indentStack.top())
+			{
+				indentStack.push(currInden);
+				showMessage("DEBUG", "INDENT", "", lineNumber, pointer / 4);
+			}
+			while (currInden < indentStack.top())
+			{
+				indentStack.pop();
+				showMessage("DEBUG", "DEDENT", "", lineNumber, pointer / 4);
+			}
+
+			while (pointer < endLine)
+			{
+				MToken currToken = getToken(line, pointer, endLine);
+				// if (identifier.length())
+				showMessage("DEBUG", currToken.type, currToken.value, lineNumber, pointer);
+
+				// char currChar = peekChar(line, pointer);
+				// cout << currChar << ' ' << int(currChar) << "\n";
+
+				currChar = getChar(line, pointer);
+			}
+			cout << "------------NEWLINE----------------\n";
+			// showMessage("DEBUG", "NEWLINE", "", lineNumber, pointer);
+			lineNumber++;
+		}
+	}
+
+	file.close();
 }
 
 int main(int argc, char *argv[])
 {
-    parse(argv[1]);
+	scan(argv[1]);
 }
