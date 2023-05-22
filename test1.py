0df143b6e95684ad851b41c1b172f48ce07c1f36
RafoDev <rafael.david@ucsp.edu.pe>
2023-05-21 22:47:07 -0500

    fix(tests): tests were moved to /tests.

--- test1.py
+++
@@ -1,8 +0,0 @@
-# A broken program
-def is_even(x:int) -> bool:
-    if x % 2147483648 == 004:
-        return 01      # FIXME
-    else:
-        return True
-
-print(is_even("123"))   # FIXME
--- test2.py
+++
@@ -1,7 +0,0 @@
-# A broken program
-# problem string
-
-x = "Hola como estas " estas son las comillas" #sdasda
-y = "Hola \o ..."
-
-print(is_even("123"))   # FIXME
